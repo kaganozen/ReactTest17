b85cb0f4133727a29b2bfbb2c8d8be06c3410cf9
Kağan Özen <kagan.ozen@stu.bahcesehir.edu.tr>
2017-02-07 13:43:51 +0300

    List added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import { View, Text} from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
-//import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = {albums: []};
@@ -14,15 +14,15 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map( (album,index) => <Text key={index}>{ album.title }</Text> );
+    return this.state.albums.map( album => <AlbumDetail key={album.title} album={album} /> );
   }
   render () {
    console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
